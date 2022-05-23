30523a9d0b486c58bb76f3707e2bca11045564a6
bitsko <26992310+bitsko@users.noreply.github.com>
2022-05-23 15:39:11 -0700

    Update dumpprivs.sh

--- dumpprivs.sh
+++ dumpprivs.sh
@@ -2,7 +2,7 @@
 
 # while read -r line; do ncli dumpprivkey "$line"; done<<<$(grep address<<<$(ncli listunspent) | sort | uniq | cut -d '"' -f 4)
 
-ncli(){ $HOME/.novo-bitcoin/bin/novobitcoin-cli $1 $2 $3 $4 $5 $6; }
+ncli(){ $HOME/.novo/bin/novo-cli $1 $2 $3 $4 $5 $6; }
 while read -r line; do
 	ncli dumpprivkey "$line"
 done<<<"$(ncli listunspent | jq -r .[].address | sort | uniq)"
