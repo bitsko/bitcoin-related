9a1bad6ea4b6cdd5b625eecede7a2b2e2b798e54
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-19 13:23:04 -0700

    Create get-bdb-5.3.sh

---
+++ get-bdb-5.3.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Installs Berkeley DB 5.3.
+# BDB 5.3 is installed locally to a new bdb/ folder.
+# @author Jack Peterson (jack@tinybike.net)
+# tampered with by bitsko for OpenBSD testing
+
+set -e
+trap "exit" INT
+
+BLUE='\033[1;34m'
+RED='\033[1;31m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+startdir=$(pwd)
+echo -e "\033[0;34mLogging to $startdir/log${NC}"
+if [ -f "log" ]; then rm log; fi
+bdb_dir="db5"
+if [ ! -d "$bdb_dir" ]; then
+    mkdir -p "$bdb_dir"
+    bdbtargz="db5.tar.gz"
+    bdbdlname="db-5.3.28.NC"
+    if [ ! -f "$startdir/$bdbtargz" ]; then
+        bdburl="http://download.oracle.com/berkeley-db/${bdbdlname}.tar.gz"
+        echo -e "${RED}Downloading $bdburl...${NC}"
+        wget -O $startdir/$bdbtargz $bdburl
+#        wget $bdburl
+    fi
+    echo -e "${BLUE}Installing Berkeley DB 5.3 to $startdir/$bdb_dir...${NC}"
+    mkdir -p "$bdb_dir"
+    echo -e "${CYAN}  - unpack $bdbtargz -> ${PWD##*/}/${bdb_dir}${NC}"
+#    gunzip "$bdbtargz"
+    tar -zxvf "$bdbtargz" -s /${bdbdlname}/${bdb_dir}/ >>$startdir/log 2>&1
+#    cp -r ${bdbdlname} ${bdb_dir}
+#    tar -zxvf ${startdir}/${bdbtargz} -C "$bdb_dir" --strip-components=1 >>$startdir/log 2>&1
+    cd "${startdir}"/"${bdb_dir}"
+
+    cd "${startdir}"/"${bdb_dir}"/build_unix
+    mkdir -p build
+    export BDB_PREFIX=$(pwd)/build
+    echo -e "${CYAN}  - dist/configure${NC}"
+    ../dist/configure --disable-shared --disable-replication --enable-cxx --with-pic --prefix=$BDB_PREFIX >>$startdir/log 2>&1
+    echo -e "${CYAN}  - install -> ${PWD##*/}/build${NC}"
+    make install >>$startdir/log 2>&1
+    cd ../..
+    echo -e "${RED}Done.${NC}"
+else
+    export BDB_PREFIX=$(pwd)/"${bdb_dir}"/build_unix/build
+    echo -e "${RED}Found local Berkeley DB: $BDB_PREFIX${NC}"
+fi
+cd "$startdir"
+# echo -e "${BLUE}Checking Berkeley DB version...${NC}"
+# g++ version.cpp -I${BDB_PREFIX}/include/ -L${BDB_PREFIX}/lib/ -o version
+# ./version
+if [ -f "$startdir/../configure.ac" ] || [ -f "$startdir/../configure.in" ]; then
+    cd "$startdir/.."
+    echo -e "${BLUE}Creating configure script...${NC}"
+    if [ ! -f "aclocal.m4" ]; then
+        ./autogen.sh >>$startdir/log 2>&1
+    fi
+    autoreconf --install --force --prepend-include=${BDB_PREFIX}/include/ >>$startdir/log 2>&1
+    echo -e "${BLUE}Configuring...${NC}"
+    ./configure CPPFLAGS="-I${BDB_PREFIX}/include/" LDFLAGS="-L${BDB_PREFIX}/lib/" >>$startdir/log 2>&1
+    cd "$startdir"
+    echo -e "${RED}Done.${NC}"
+fi
