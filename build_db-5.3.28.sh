f259fba64cc40dfcfde1a3c820b58d393447a8bb
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-21 01:17:17 -0700

    Update build_db-5.3.28.sh

--- build_db-5.3.28.sh
+++ build_db-5.3.28.sh
@@ -2,4 +2,12 @@ wget https://github.com/berkeleydb/libdb/releases/download/v5.3.28/db-5.3.28.tar
 
 tar -zxvf db-5.3.28.tar.gz
 
-cd db-5.3.28
+cd db-5.3.28/build_unix
+
+../dist/configure
+
+mkdir build
+
+cd build
+
+../make
