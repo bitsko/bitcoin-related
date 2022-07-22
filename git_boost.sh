2ee657b13e780f65aaf11d2fb6155bfaf9f2146a
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-22 10:01:55 -0700

    Update git_boost.sh

--- git_boost.sh
+++ git_boost.sh
@@ -1,5 +1,6 @@
 git clone --recursive https://github.com/boostorg/boost.git
 cd boost
-git checkout develop # or whatever branch you want to use
+git checkout tags/boost-1.61.0 -b master
+# git checkout develop # or whatever branch you want to use
 ./bootstrap.sh
 ./b2 headers
