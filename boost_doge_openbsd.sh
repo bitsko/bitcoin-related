7edb394f5e55f443b8e7e35fae76e756946d55b6
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-21 21:20:44 -0700

    Update boost_doge_openbsd.sh

--- boost_doge_openbsd.sh
+++ boost_doge_openbsd.sh
@@ -3,10 +3,12 @@ BITCOIN_ROOT=$(pwd)
 BOOST_PREFIX="${BITCOIN_ROOT}/boost"
 mkdir -p $BOOST_PREFIX
 
-wget https://github.com/boostorg/boost/archive/refs/tags/boost-1.79.0.tar.gz
-tar -zxvf boost-1.79.0.tar.gz
-
-cd boost-boost-1.79.0
+# wget https://github.com/boostorg/boost/archive/refs/tags/boost-1.79.0.tar.gz
+wget https://github.com/boostorg/boost/archive/refs/tags/boost-1.61.0.tar.gz
+# tar -zxvf boost-1.79.0.tar.gz
+tar -zxvf boost-1.61.0.tar.gz
+# cd boost-boost-1.79.0
+cd boost-boost-1.61.0
 
 # Build w/ minimum configuration necessary for dogecoin
 echo 'using gcc : : eg++ : <cxxflags>"-fvisibility=hidden -fPIC" <linkflags>"" <archiver>"ar" <striper>"strip"  <ranlib>"ranlib" <rc>"" : ;' > user-config.jam
